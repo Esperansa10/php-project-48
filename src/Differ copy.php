64ef242f651987f871b449d81551152ad335f099
Nadin <esperansa10@gmail.com>
2024-03-27 12:54:53 +0300

    2 project

--- src/Differ copy.php
+++ src/Differ copy.php
@@ -4,6 +4,7 @@ namespace MyApp\Differ;
 
 function genDiff($file1, $file2) {
 
+  $diff = []; 
 
   $arr1 = json_decode($file1, true); 
   $arr2 = json_decode($file2, true); 
@@ -12,21 +13,32 @@ function genDiff($file1, $file2) {
 foreach($arr1 as $key => $value) {
   if (array_key_exists($key, $arr2)) { 
     if ($arr1[$key] === $arr2[$key]) {
-      echo $key . ': ' . $arr1[$key] . "\n" ; 
+      // echo $key . ': ' . $arr1[$key] . "\n" ; 
+      // $diff[] = $key . ': ' . $arr1[$key] . "\n" ; 
+      $diff[$key] = $arr1[$key]; 
     }
     else {
-      echo '- ' . $key. ': ' . $arr1[$key]."\n" ;
-      echo'+ ' . $key. ': ' . $arr2[$key]. "\n" ;
+      // echo '- ' . $key. ': ' . $arr1[$key]."\n" ;
+      // echo'+ ' . $key. ': ' . $arr2[$key]. "\n" ;
+      $diff['- ' . $key] = $arr1[$key]; 
+     
+      $diff['+ ' .$key] = $arr2[$key]; 
+      ;
+      
     } 
 
   }
   else {
     
     if($arr1[$key] === false) {
-      echo  '- ' . $key. ': ' . "false \n" ;
+      // echo  '- ' . $key. ': ' . "false \n" ;
+      $diff['- ' .$key] = 'false';
+      
     }
     else {
-      echo '- ' . $key. ': ' . $arr1[$key]."\n" ;
+      // echo '- ' . $key. ': ' . $arr1[$key]."\n" ;
+      $diff['- ' .$key] = $arr1[$key]; 
+      
     }
     
 
@@ -36,10 +48,14 @@ foreach($arr1 as $key => $value) {
 foreach($arr2 as $key => $value) {
   if (!array_key_exists($key, $arr1)) {
     if($arr2[$key] === true) {
-      echo  '+ ' . $key. ': ' . "true \n" ;  
+      // echo  '+ ' . $key. ': ' . "true \n" ; 
+      $diff['+ ' .$key] = 'true'; 
+     
     } 
     else {
-    echo '+ ' . $key. ': ' . $arr2[$key]."\n" ;
+    // echo '+ ' . $key. ': ' . $arr2[$key]."\n" ;
+    $diff[$key] = $arr2[$key]; 
+  
   }
   }
   
@@ -47,4 +63,6 @@ foreach($arr2 as $key => $value) {
 
 
 // print_r(json_encode($diff)); 
+// ksort($diff);
+print_r($diff); 
 }
--- src/Differ.php
+++ src/Differ.php
@@ -4,60 +4,83 @@ namespace MyApp\Differ;
 
 function genDiff($file1, $file2) {
 
-  $diff = []; 
+  $key = ''; 
+  $value = ''; 
+  $compare = ''; 
+
+  $diff =  [
+    'key' => $key,
+    'value' => $value,
+    'compare' => $compare
+  ]; 
 
   $arr1 = json_decode($file1, true); 
   $arr2 = json_decode($file2, true); 
   
    
-foreach($arr1 as $key => $value) {
+foreach($arr1 as $key => $value) { //"host": "hexlet.io",
+ 
+  $diff['key'] = $arr1[$key]; 
+  $diff['value'] = $arr1[$value]; 
+  $diff['compare'] = 'test'; 
+
   if (array_key_exists($key, $arr2)) { 
+
+    
     if ($arr1[$key] === $arr2[$key]) {
       // echo $key . ': ' . $arr1[$key] . "\n" ; 
       // $diff[] = $key . ': ' . $arr1[$key] . "\n" ; 
-      $diff[$key] = $arr1[$key]; 
+      // $diff['key'] = $arr1[$key]; 
+      // $diff['value'] = $arr1[$value]; 
+      // $diff['compare'] = 'test'; 
     }
     else {
       // echo '- ' . $key. ': ' . $arr1[$key]."\n" ;
       // echo'+ ' . $key. ': ' . $arr2[$key]. "\n" ;
-      $diff['- ' . $key] = $arr1[$key]; 
+      
+    //  $diff[$key] = $arr1[$key]; 
+    //  $diff[$compare] = '-'; 
+    //  $diff[$key] = $arr2[$key]; 
+    //  $diff[$compare] = '+'; 
      
-      $diff['+ ' .$key] = $arr2[$key]; 
-      ;
+    //   ;
       
     } 
 
   }
   else {
     
-    if($arr1[$key] === false) {
-      // echo  '- ' . $key. ': ' . "false \n" ;
-      $diff['- ' .$key] = 'false';
+  //   if($arr1[$key] === false) {
+  //     // echo  '- ' . $key. ': ' . "false \n" ;
+  //     $diff[$key] = 'false';
+  //     $diff[$compare] = '-'; 
       
-    }
-    else {
-      // echo '- ' . $key. ': ' . $arr1[$key]."\n" ;
-      $diff['- ' .$key] = $arr1[$key]; 
+  //   }
+  //   else {
+  //     // echo '- ' . $key. ': ' . $arr1[$key]."\n" ;
+  //     $diff[$key] = $arr1[$key]; 
+  //     $diff[$compare] = '-'; 
       
-    }
+  //   }
     
 
-  }
+  // }
 }
 
-foreach($arr2 as $key => $value) {
-  if (!array_key_exists($key, $arr1)) {
-    if($arr2[$key] === true) {
-      // echo  '+ ' . $key. ': ' . "true \n" ; 
-      $diff['+ ' .$key] = 'true'; 
+// foreach($arr2 as $key => $value) {
+//   if (!array_key_exists($key, $arr1)) {
+//     if($arr2[$key] === true) {
+//       // echo  '+ ' . $key. ': ' . "true \n" ; 
+//       $diff[$key] = 'true'; 
+//       $diff[$compare] = '+'; 
      
-    } 
-    else {
-    // echo '+ ' . $key. ': ' . $arr2[$key]."\n" ;
-    $diff[$key] = $arr2[$key]; 
+//     } 
+//     else {
+//     // echo '+ ' . $key. ': ' . $arr2[$key]."\n" ;
+//     $diff[$key] = $arr2[$key]; 
   
-  }
-  }
+//   }
+//   }
   
 }
 
